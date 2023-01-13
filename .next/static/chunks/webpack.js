c3c1cb1af442b8cbbe44371d094d4909943c1255
Teodor <teodor.v.milchev@gmail.com>
2023-01-13 17:33:52 +0000

    Header/Footer/SpeakersList

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "21bd22a313ff8cac"; }
+/******/ 		__webpack_require__.h = function() { return "03426e336737ac89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/components/SpeakersToolbar.js
@@ -0,0 +1,36 @@
+function SpeakersToolbar() {
+    return (
+        <section className="toolbar dark-theme-header">
+          <div className="container">
+            <div className="justify-content-between">
+              <ul className="toolrow d-flex flex-column flex-lg-row">
+                <li className="d-lex flex-column flex-md-row">
+                  <b>Show Sessions &nbsp;&nbsp;</b>
+                  <label className="fav">
+                    <input
+                    type="checkbox"
+                    checked={true}
+                    />
+                    <span className="switch"></span>
+                  </label>
+                </li>
+                <li className="d-flex flex-column flex-md-row ml-sm-5 ml-0">
+                  <strong>Theme</strong>
+                  <label className="dropdown">
+                    <select 
+                    className="form-control theme"
+                    value="Light"
+                    >
+                      <option value="Light">Light</option>
+                      <option value="Dark">Dark</option>
+                    </select>
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+    )
+}
+
+export default SpeakersToolbar;
